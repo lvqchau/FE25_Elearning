c47c6ff24050b28b1c8db3c82b5ae7f5fa082b34
Hoang <lamquochoang1995@gmail.com>
2019-10-28 16:18:07 +0700

    MaterialHeader

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -1,55 +1,79 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { connect } from "react-redux";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button
+} from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const styles = {
+  title: {
+    flexGrow: 1
+  }
+};
 
 const Header = props => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <a className="navbar-brand" href="#">
-        E-learning
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <NavLink
-              activeStyle={{ color: "red" }}
-              exact
-              to="/"
-              className="nav-link"
-            >
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              activeStyle={{ color: "red" }}
-              to="/signin"
-              className="nav-link"
-            >
-              Login
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              activeStyle={{ color: "red" }}
-              to="/signup"
-              className="nav-link"
-            >
-              Signup
-            </NavLink>
-          </li>
-        </ul>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h5" style={styles.title}>
+          E-learning
+        </Typography>
+        {/* <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button> */}
+        {/* <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item"> */}
+        <Button color="inherit">
+          <NavLink
+            activeStyle={{ color: "red" }}
+            exact
+            to="/"
+            className="nav-link"
+          >
+            Home
+          </NavLink>
+        </Button>
+        {/* </li> */}
+        {/* <li className="nav-item"> */}
+        <Button color="inherit">
+          <NavLink
+            activeStyle={{ color: "red" }}
+            to="/signin"
+            className="nav-link"
+          >
+            Login
+          </NavLink>
+        </Button>
+        {/* </li> */}
+        {/* <li className="nav-item"> */}
+        <Button color="inherit">
+          <NavLink
+            activeStyle={{ color: "red" }}
+            to="/signup"
+            className="nav-link"
+          >
+            Signup
+          </NavLink>
+        </Button>
+        {/* </li> */}
+        {/* </ul> */}
         {props.credentials && (
           <div>
             <span className="nav-link text-white">
@@ -57,8 +81,9 @@ const Header = props => {
             </span>
           </div>
         )}
-      </div>
-    </nav>
+        {/* </div> */}
+      </Toolbar>
+    </AppBar>
   );
 };
 
