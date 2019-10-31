8025c21ca004999daa85028ecb4114ed75f313ba
Dinh Hung <hungng147147@gmail.com>
2019-10-31 15:41:33 +0700

    Header

--- src/Layouts/Header.js
+++ src/Layouts/Header.js
@@ -1,67 +1,93 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
 import { connect } from "react-redux";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import MenuIcon from "@material-ui/icons/Menu";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
 
-const Header = props => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(3),
+      width: "auto"
+    }
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  inputRoot: {
+    color: "inherit"
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: 700
+    }
+  }
+}));
+
+export default function ButtonAppBar() {
+  const classes = useStyles();
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <Link className="navbar-brand" to='/'>
-        E-learning
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item">
-            <NavLink
-              activeStyle={{ color: "red" }}
-              exact
-              to="/"
-              className="nav-link"
-            >
-              Home
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              activeStyle={{ color: "red" }}
-              to="/signin"
-              className="nav-link"
-            >
-              Login
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink
-              activeStyle={{ color: "red" }}
-              to="/signup"
-              className="nav-link"
-            >
-              Signup
-            </NavLink>
-          </li>
-        </ul>
-        {props.credentials && (
-          <div>
-            <span className="nav-link text-white">
-              Hi, {props.credentials.hoTen}
-            </span>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <i class="fab fa-dev"></i>
+          <Typography variant="h4" className={classes.title}>
+            DevGarden
+          </Typography>
+          <MenuIcon />
+          <Button color="inherit">Danh sách khóa học</Button>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
           </div>
-        )}
-      </div>
-    </nav>
-  );
-};
 
-export default connect(state => ({
-  credentials: state.user.credentials
-}))(Header);
+          <Button color="inherit">SignUp</Button>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
